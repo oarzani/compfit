749578dc5edd78880a539091a71b9813287c6264
Omid Arzani <omid.arzani@gmx.net>
2019-10-22 10:13:08 +0200

    First try of an backend

---
+++ src/api/filters.js
@@ -0,0 +1,15 @@
+export const NeighbourhoodOptions = [
+  "Ehrenfeld",
+  "Innenstadt",
+  "Alstadt-Süd",
+  "Alstadt-Nord"
+];
+
+export const StudioOptions = [
+  "Cardio",
+  "Courses",
+  "Ladyarea",
+  "Personal-Training",
+  "Strength",
+  "Wellness"
+];
