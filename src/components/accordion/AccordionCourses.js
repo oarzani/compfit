14ba2d4f8115926ac70b736d5d97546f68530481
Omid Arzani <omid.arzani@gmx.net>
2019-11-02 14:09:04 +0100

    add misc

---
+++ src/components/accordion/AccordionCourses.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import OpenAccordion from "../icons/OpenAccordion";
+import { ListItem } from "./AccordionCardio";
+
+const AccordionWrapper = styled.div`
+  width: 100%;
+  /* min-height: 43px; */
+`;
+
+const AccordionDiv = styled.div`
+  background: transparent;
+  color: #4ecbe0;
+  /* display: flex;
+  justify-content: center; */
+  text-align: center;
+
+  box-shadow: 2px 2px 5px rgb(112, 112, 112);
+  align-items: center;
+  font-size: 16pt;
+  padding: 4px;
+  font-family: Arial;
+`;
+
+const Content = styled.div`
+  display: ${props => (props.show ? "block" : "none")};
+  width: 100%;
+  color: #707070;
+`;
+
+const Arrow = styled.div`
+  align-self: right;
+  transform: ${props => (props.show ? "rotate(180deg)" : "rotate(0deg)")};
+  transition: all 0.2s;
+`;
+
+export default function AccordionCourses({ courses }) {
+  const [toggleContent, setToggleContent] = useState(false);
+
+  console.log(toggleContent);
+  function handleClick() {
+    setToggleContent(!toggleContent);
+  }
+  return (
+    <>
+      <AccordionWrapper>
+        <AccordionDiv onClick={handleClick}>
+          Kurse
+          <Arrow show={toggleContent}>
+            <OpenAccordion />
+          </Arrow>
+        </AccordionDiv>
+        <Content show={toggleContent}>
+          {courses.map(courses => {
+            return <ListItem>{courses}</ListItem>;
+          })}
+        </Content>
+      </AccordionWrapper>
+    </>
+  );
+}
