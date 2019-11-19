cda8bf446227bf563f7712041f0052003146e71c
oarzani <dimo@DESKTOP-GKJ2QTF.localdomain>
2019-11-19 19:39:10 +0100

    cleaned code

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -22,29 +22,3 @@ export const motion = keyframes`
   100%   { opacity:1; }
 `;
 
-// export const flickerAlter = keyframes`
-//   0%   { opacity:1; }
-//   6%   { opacity:1; }
-//   7.5%   { opacity:1; }
-//   10.5%   { opacity:1; }
-//   11%   { opacity:1; }
-//   12%   { opacity:1;}
-//   45%   { opacity:1; }
-//   50%   { opacity:1; }
-//   51%   { opacity:0; }
-//   51.5%   { opacity:1; }
-//   52%   { opacity:0; }
-//   52.5%   { opacity:1; }
-//   64%   { opacity:0; }
-//   64.5%   { opacity:1; }
-//   65%   { opacity:1; }
-//   70%   { opacity:1; }
-//   80%   { opacity:1; }
-//   85%   { opacity:1; }
-//   90%   { opacity:1; }
-//   90.5%   { opacity:0; }
-//   91%   { opacity:1; }
-//   91.5%   { opacity:0; }
-//   92%   { opacity:1; }
-//   100%   { opacity:1; }
-// `;
